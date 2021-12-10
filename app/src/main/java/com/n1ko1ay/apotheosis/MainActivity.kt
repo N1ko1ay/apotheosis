3c0f1261ccd6762bf176ce52792f851c23d6dd55
N1ko1ay <gastaclaus63@gmail.com>
2021-12-10 13:03:51 +0400

    Material

---
+++ app/src/main/java/com/n1ko1ay/apotheosis/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.n1ko1ay.apotheosis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationMenu: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavigationMenu = findViewById(R.id.bottom_navigation_menu)
+    }
+}
